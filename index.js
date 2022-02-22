6d8a96e1a0e8728996e10df1c57eac53939a3694
evgeniy <evgeniy@gitlab@sv.skyflabs.ru>
2022-02-22 10:42:39 +0300

    Main endpoint

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+const config = require("./config");
+const metrics = require("./lib/metrics");
+const click = require("./lib/click");
+const server = require("./lib/server");
+
+
+server.listen(config.server.port, async () => {
+  console.log("Start exporter");
+  return undefined;
+});
